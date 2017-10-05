117d58fdcbcc693e0cfce4f36c043f43b11e8c9b
YaroslavW <doctor355@yahoo.com>
2017-10-05 23:42:16 +0300

    First commit

---
+++ peopleCom.js
@@ -0,0 +1,18 @@
+// var people;
+// var communication;
+
+ function countCommunication(p) {
+var c=0;
+ p =p - 1;
+ while(p > 0) {
+ c += p;
+ p--;
+ }
+ console.log(c);
+ }
+
+ countCommunication(12);
+ countCommunication(13);
+ countCommunication(14);
+ countCommunication(15);
+ countCommunication(16);
