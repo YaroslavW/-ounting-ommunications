06fb6e7e8fc5d9dc63741ce943c16facb6257400
YaroslavW <doctor355@yahoo.com>
2017-10-06 00:25:34 +0300

    ready file

--- peopleCom.js
+++ peopleCom.js
@@ -1,18 +1,22 @@
-// var people;
-// var communication;
+
 
  function countCommunication(p) {
 var c=0;
+if (p <= 3){
+	console.log(p);
+} else {
+
  p =p - 1;
- while(p > 0) {
+ while(p > 0) { 
+
  c += p;
  p--;
  }
  console.log(c);
- }
-
- countCommunication(12);
- countCommunication(13);
- countCommunication(14);
- countCommunication(15);
- countCommunication(16);
+} 
+}
+ countCommunication(7);
+ countCommunication(8);
+ countCommunication(3);
+ countCommunication(2);
+ countCommunication(1);
